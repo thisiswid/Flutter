f1cc003b827c16ae18c729acf5b8cf20946c0424
Widi <widiyamail5@gmail.com>
2024-05-21 21:54:39 +0700

    publish

--- aplikasi/lib/aplikasi/publish.dart
+++ aplikasi/lib/aplikasi/publish.dart
@@ -7,19 +7,115 @@ class PublishScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(
-          'Publish Story',
+          'Book',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          'This is the Publish Screen',
-          style: TextStyle(fontSize: 18),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.asset('assets/images/buku3.png'),
+                ),
+                Text(
+                  'Title Book',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Sesuap Rasa',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Synopsis',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Seseorang yang memberanikan diri untuk study di negara yang asing akan islam.',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Gendre',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Romantis',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Divider(
+                  thickness: 2,
+                  color: Colors.black,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title Story Part',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ), // Add spacing between the text and the dots
+                    Row(
+                      children: [
+                        Text(
+                          'Prolog - Upload 2 April 2024',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ), // Add spacing between the text and the dots
+                        Spacer(), // Spacer to push the icon to the right
+                        Icon(Icons.more_vert), // Three dots icon
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(),
+          ),
+        ],
       ),
     );
   }
